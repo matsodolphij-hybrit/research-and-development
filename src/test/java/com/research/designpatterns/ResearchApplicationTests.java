230951a2a56625f243c6e8a99402ec2c2dea790c
Mats Odolphij <mats.odolphij@hybrit.org>
2024-02-20 16:05:05 +0100

    created a bare project with jdk 21 for creating and testing design patterns

---
+++ src/test/java/com/research/designpatterns/ResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
