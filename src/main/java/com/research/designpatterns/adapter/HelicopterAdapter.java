06f1ef449e5e61e01fe200f0ffa099d519a08cd2
Mats Odolphij <mats.odolphij@hybrit.org>
2024-03-11 11:23:32 +0100

    added some javadoc for the adapter pattern

--- src/main/java/com/research/designpatterns/adapter/HelicopterAdapter.java
+++ src/main/java/com/research/designpatterns/adapter/HelicopterAdapter.java
@@ -1,5 +1,12 @@
 package com.research.designpatterns.adapter;
 
+/**
+ * What is the adapter design pattern?
+ * 1. it allows 2 incompatible interfaces to work together
+ * 2. Used when the client expects a (target) interface
+ * 3. The adapter class allows the use of the available interface and the target interface.
+ * 4. Any class can work together as long as the adapter solves the issue that all classes must implement every method defined by the shared interface.
+ */
 public class HelicopterAdapter implements Vehicle {
 
     private Helicopter helicopter;
